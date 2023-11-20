462a8274b1bd8893b700897fdab3df17a3fe6caf
Antonio Šumak <sumakantonio@gmail.com>
2023-11-20 10:35:05 +0100

    Remove image package

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,12 @@ import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import image from '@astrojs/image';
-import astroI18next from 'astro-i18next';
-
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind(), image(), astroI18next()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  experimental: { assets: true },
 });
